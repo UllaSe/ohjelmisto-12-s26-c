94a27335ffc5b3ab3acc432de9f51dfd2414041a
Ulla Sederlöf <ulla.sederlof@metropolia.fi>
2026-08-18 15:32:34 +0300

    initial commit

---
+++ mod01/hello.py
@@ -0,0 +1,6 @@
+# tämä on esimmäinen koodini
+print('Moikka maailm, nimeni on Ulla!!')
+print('Kiva nähdä')
+
+name = input('Mikä sinun nimesi on: ')
+print('Terve', name)
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Ohjelmisto 1 - Python harjoitukset
+
+**Ulla Sederlöf**
+
+## Moduuli 1 ja 2
+
+Tein tehtävät 1 ja 2.
