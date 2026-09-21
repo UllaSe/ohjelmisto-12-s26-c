0e82e799193aaff346fd8813fe38728f0da651e8
mattpe <mattpe@metropolia.fi>
2026-09-21 10:39:10 +0300

    add: more examples

--- mod10/mod10-esim.py
+++ mod10/mod10-esim.py
@@ -26,9 +26,10 @@ class Koira:
 
 class Hoitola:
 
-    def __init__(self):
+    def __init__(self, osoite):
         # tässä asossiaatio listan avulla
         self.koirat = []
+        self.osoite = osoite
 
     def koira_sisään(self, koira):
         self.koirat.append(koira)
@@ -36,12 +37,38 @@ class Hoitola:
         print(koira.nimi + ' kirjattu sisään')
         # hotola pääsee myös kutsumaan koiran metodeja
         # tämäkin on assosiaatio eli hoitola "tuntee" toisen olion 
-        print(koira.hauku(2))
+        koira.hauku(2)
 
 
 koira1 = Koira("Muro", 2018)
 koira2 = Koira("Rekku", 2022, "Viu viu viu")
 
-hoitola = Hoitola()
+hoitola = Hoitola("Mäkitie 5")
 hoitola.koira_sisään(koira1)
 hoitola.koira_sisään(koira2)
+
+koira1.hauku(3)
+koira2.hauku(2)
+koira2 = koira1 # viittaus ensimmäiseen koiraan poistuu ja kummatkin muuttujaat viittavat samaan olioon
+koira2.hauku(1)
+
+# Luodaan kolmas koira ja sijoitetaan se suoraan hoitolaan
+hoitola.koira_sisään(Koira("Bella", 2016))
+
+# olion ominaisuuksiin voidaan viitata pythonissa myös suoraan
+hoitola.koirat[0].hauku(2)
+
+
+
+
+
+## Lista on myös olio ja siihen viitataan muuttujilla
+"""
+def muokkaa_listaa(muokattava_lista):
+    muokattava_lista.append(6)
+
+lista = [1,5,8]
+print(lista)
+muokkaa_listaa(lista)
+print(lista)
+"""
